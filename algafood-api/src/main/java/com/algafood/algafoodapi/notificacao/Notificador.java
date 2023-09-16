b038c049b3b4ddccffb4d0c4134db5b9f0705d48
aldeir <dide123francisco@gmail.com>
2023-09-16 20:34:51 -0300

    limpando e criando entity cozinha e restaurante

--- algafood-api/src/main/java/com/algafood/algafoodapi/notificacao/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.algafood.algafoodapi.notificacao;
-
-import com.algafood.algafoodapi.modelo.Cliente;
-
-public interface Notificador {
-
-    void notificar(Cliente cliente, String mensagem);
-
-}
