c6b61f1ef67eda0a460c7ce9572549390716a671
aldeir <dide123francisco@gmail.com>
2023-09-26 15:55:14 -0300

    Modelando e implementando a atualização de recursos com PUT

--- algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.algafood.algafoodapi.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,12 +11,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algafood.algafoodapi.domain.repository.CozinhaRepository;
+
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
+
 import com.algafood.algafoodapi.domain.model.Cozinha;
 
 //@ResponseBody garante que a resposta do metodo vai ser o corpo da requeiscao
@@ -35,10 +40,10 @@ public class CozinhaController {
     // @ResponseStatus(HttpStatus.CREATED)
     @GetMapping("/{cozinhaId}") // @PathVariable("cozinhaId") long id
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-        Cozinha conzinha = cozinhaRepository.buscar(cozinhaId);
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 
-        if (conzinha != null) {
-            return ResponseEntity.ok(conzinha);
+        if (cozinha != null) {
+            return ResponseEntity.ok(cozinha);
         }
 
         return ResponseEntity
@@ -49,6 +54,20 @@ public class CozinhaController {
     @ResponseStatus(HttpStatus.CREATED)
     public void adicionar(@RequestBody Cozinha cozinha) {
         cozinhaRepository.adicionar(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
+            @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+        if (cozinhaAtual != null) {
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+            cozinhaRepository.adicionar(cozinhaAtual);
+            return ResponseEntity.ok().body(cozinhaAtual);
+        }
+        return ResponseEntity
+                .notFound().build();
     }
 
 }
