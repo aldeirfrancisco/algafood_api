9edb938cd4907fe27abff3bdcd75babbb414a620
aldeir <dide123francisco@gmail.com>
2023-09-26 15:16:56 -0300

    Modelando e implementando a inclusão de recursos com POST

--- algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/CozinhaController.java
@@ -9,6 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,20 +36,19 @@ public class CozinhaController {
     @GetMapping("/{cozinhaId}") // @PathVariable("cozinhaId") long id
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
         Cozinha conzinha = cozinhaRepository.buscar(cozinhaId);
-        // return ResponseEntity.status(HttpStatus.OK).body(conzinha);
-        // return ResponseEntity.ok(conzinha);
-        // HttpHeaders headers = new HttpHeaders();
-        // headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
-        // return ResponseEntity
-        // .status(HttpStatus.FOUND)
-        // .headers(headers)
-        // .build();
+
         if (conzinha != null) {
             return ResponseEntity.ok(conzinha);
         }
 
         return ResponseEntity
                 .notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void adicionar(@RequestBody Cozinha cozinha) {
+        cozinhaRepository.adicionar(cozinha);
     }
 
 }
