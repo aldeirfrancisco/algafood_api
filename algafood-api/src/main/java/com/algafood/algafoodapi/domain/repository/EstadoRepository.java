4002bb8f6d4f5cbe2a4dc5f196deca47dbda8b2e
aldeir <dide123francisco@gmail.com>
2023-10-02 10:31:56 -0300

    Refatorando o código do projeto para usar o repositório do SDJ.

--- algafood-api/src/main/java/com/algafood/algafoodapi/domain/repository/EstadoRepository.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/domain/repository/EstadoRepository.java
@@ -2,16 +2,12 @@ package com.algafood.algafoodapi.domain.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.algafood.algafoodapi.domain.model.Estado;
 
-public interface EstadoRepository {
-
-    List<Estado> listar();
-
-    Estado buscar(Long id);
-
-    Estado salvar(Estado estado);
-
-    void remover(Long estadoId);
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
 
 }
