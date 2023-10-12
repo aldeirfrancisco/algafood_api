811508bf237dfde9b4e64619b553c928ec32df84
aldeir <dide123francisco@gmail.com>
2023-10-12 11:06:08 -0300

    mapeando relacionamentos muitos-para-muitos grupos e usuario

--- algafood-api/src/main/java/com/algafood/algafoodapi/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/domain/model/Restaurante.java
@@ -11,7 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumns;
+
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
