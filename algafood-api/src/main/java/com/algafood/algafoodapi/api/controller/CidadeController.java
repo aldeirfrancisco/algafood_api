1fce0721dbfbb8dafedce38e120b427af2598641
aldeir <dide123francisco@gmail.com>
2023-10-21 11:17:13 -0300

    refatorando o service e a api da cidade.

--- algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/CidadeController.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/CidadeController.java
@@ -38,42 +38,25 @@ public class CidadeController {
     }
 
     @GetMapping("/{cidadeId}")
-    public ResponseEntity<Cidade> buscar(@PathVariable Long cidadeId) {
-        Optional<Cidade> cidade = cidadeRepository.findById(cidadeId);
+    public Cidade buscar(@PathVariable Long cidadeId) {
+        return cadastroCidade.buscarOuFalhar(cidadeId);
 
-        if (cidade.isPresent()) {
-            return ResponseEntity.ok(cidade.get());
-        }
-
-        return ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public ResponseEntity<?> adicionar(@RequestBody Cidade cidade) {
-        try {
-            cidade = cadastroCidade.salvar(cidade);
-
-            return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(cidade);
-        } catch (EntidadeNaoEncontradaException e) {
-            return ResponseEntity.badRequest()
-                    .body(e.getMessage());
-        }
+    public Cidade adicionar(@RequestBody Cidade cidade) {
+        return cadastroCidade.salvar(cidade);
     }
 
     @PutMapping("/{cidadeId}")
-    public ResponseEntity<Cidade> atualizar(@PathVariable Long cidadeId,
+    public Cidade atualizar(@PathVariable Long cidadeId,
             @RequestBody Cidade cidade) {
-        Optional<Cidade> cidadeAtual = cidadeRepository.findById(cidadeId);
+        Cidade cidadeAtual = cadastroCidade.buscarOuFalhar(cidadeId);
 
-        if (cidadeAtual != null) {
-            BeanUtils.copyProperties(cidade, cidadeAtual.get(), "id");
+        BeanUtils.copyProperties(cidade, cidadeAtual, "id");
 
-            Cidade cidadeSalva = cadastroCidade.salvar(cidadeAtual.get());
-            return ResponseEntity.ok(cidadeSalva);
-        }
+        return cadastroCidade.salvar(cidadeAtual);
 
-        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{cidadeId}")
