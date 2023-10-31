589752ee78cb408dee0374e112827e7a78886afa
aldeir <dide123francisco@gmail.com>
2023-10-30 22:30:35 -0300

    tratando a PropertyBindingException na desserialização

--- algafood-api/src/main/java/com/algafood/algafoodapi/api/exceptionHandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/api/exceptionHandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.algafood.algafoodapi.api.exceptionHandler;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -15,7 +16,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.algafood.algafoodapi.domain.Exception.EntidadeEmUsoException;
 import com.algafood.algafoodapi.domain.Exception.EntidadeNaoEncontradaException;
 import com.algafood.algafoodapi.domain.Exception.NegocioException;
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -27,6 +30,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
                 if (rootCause instanceof InvalidFormatException) {
                         return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status,
+                                        request);
+                } else if (rootCause instanceof PropertyBindingException) {
+                        return handlePropertyBindingException((PropertyBindingException) rootCause, headers, status,
                                         request);
                 }
 
@@ -94,6 +100,22 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 return super.handleExceptionInternal(ex, body, headers, status, request);
         }
 
+        private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException ex,
+                        HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+                // Criei o método joinPath para reaproveitar em todos os métodos que precisam
+                // concatenar os nomes das propriedades (separando por ".")
+                String path = joinPath(ex.getPath());
+
+                ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+                String detail = String.format("A propriedade '%s' não existe. "
+                                + "Corrija ou remova essa propriedade e tente novamente.", path);
+
+                Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+                return handleExceptionInternal(ex, problem, headers, status, request);
+        }
+
         private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex,
                         HttpHeaders headers, HttpStatus status, WebRequest request) {
 
@@ -119,5 +141,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                                 .type(problemType.getUri())
                                 .title(problemType.getTitle())
                                 .detail(detail);
+        }
+
+        private String joinPath(List<Reference> references) {
+                return references.stream()
+                                .map(ref -> ref.getFieldName())
+                                .collect(Collectors.joining("."));
         }
 }
