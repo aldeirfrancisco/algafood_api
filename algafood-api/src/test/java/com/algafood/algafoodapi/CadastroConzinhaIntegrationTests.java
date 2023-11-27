442861f8c8bbeb2650cc478f54a13fe874eae5fa
aldeir <dide123francisco@gmail.com>
2023-11-27 11:50:10 -0300

    escrevendo testes de integração para excluir cozinha em uso e cozinha que não  existe.

--- algafood-api/src/test/java/com/algafood/algafoodapi/CadastroConzinhaIntegrationTests.java
+++ algafood-api/src/test/java/com/algafood/algafoodapi/CadastroConzinhaIntegrationTests.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.algafood.algafoodapi.domain.Exception.CozinhaNaoEncontradaException;
+import com.algafood.algafoodapi.domain.Exception.EntidadeEmUsoException;
 import com.algafood.algafoodapi.domain.model.Cozinha;
 import com.algafood.algafoodapi.domain.service.CadastroCozinhaService;
 import static org.assertj.core.api.Assertions.assertThat;
 import javax.validation.ConstraintViolationException;
 
+//JUnit 5
 //@RunWith(SpringRunner.class) levanta o contexto do spring na hora de rodar o teste
 //@SpringBootTest fornece as funcionalidade do spring boot no teste.
 @SpringBootTest
@@ -44,6 +47,32 @@ public class CadastroConzinhaIntegrationTests {
         });
 
         // validação
+        assertThat(erroEsperado).isNotNull();
+    }
+
+    @Test
+    public void deveFalhar_quandoExcluirCozinhaEmUso() {
+        EntidadeEmUsoException erroEsperado = Assertions.assertThrows(EntidadeEmUsoException.class,
+                () -> {
+                    cadastroCozinha.excluir(1L);
+                });
+        assertThat(erroEsperado).isNotNull();
+    }
+
+    /*
+     * JUnit 4
+     * 
+     * @Test(expected = CozinhaNaoEncontradaException.class)
+     * public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+     * cadastroCozinha.excluir(100L);
+     * }
+     */
+    @Test
+    public void deveFalhar_quandoExcluirCozinhaInexistente() {
+        CozinhaNaoEncontradaException erroEsperado = Assertions.assertThrows(CozinhaNaoEncontradaException.class,
+                () -> {
+                    cadastroCozinha.excluir(10L);
+                });
         assertThat(erroEsperado).isNotNull();
     }
 }
