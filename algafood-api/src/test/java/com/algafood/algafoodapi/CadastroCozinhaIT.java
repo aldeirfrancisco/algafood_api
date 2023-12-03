fba151efc26214d3480165b1083a4a71b3689c31
aldeir <dide123francisco@gmail.com>
2023-12-02 22:20:00 -0300

    Implementando Testes de API com REST Assured e validando o código de status HTTP

---
+++ algafood-api/src/test/java/com/algafood/algafoodapi/CadastroCozinhaIT.java
@@ -0,0 +1,35 @@
+package com.algafood.algafoodapi;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+//webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT =>  vai levantar um servidor web para os teste da API.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CadastroCozinhaIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        // RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); se a
+        // quando a validação falhar, vai mostra no log da requisição enviada e a
+        // responta.
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+}
