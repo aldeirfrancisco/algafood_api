d143cdeae09fe6610431ce982046c4db9594e984
aldeir <dide123francisco@gmail.com>
2023-12-02 22:37:18 -0300

    Validando o corpo da resposta HTTP

--- algafood-api/src/test/java/com/algafood/algafoodapi/CadastroCozinhaIT.java
+++ algafood-api/src/test/java/com/algafood/algafoodapi/CadastroCozinhaIT.java
@@ -1,9 +1,12 @@
 package com.algafood.algafoodapi;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -30,6 +33,23 @@ public class CadastroCozinhaIT {
                 .get()
                 .then()
                 .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveConter4Cozinha_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .body("", hasSize(4))
+                .body("nome", hasItems("Indiana", "Tailandesa"));
+        // Matchers biblioteca de escrever expressões com regra de correspondencia entre
+        // objetos.
     }
 
 }
