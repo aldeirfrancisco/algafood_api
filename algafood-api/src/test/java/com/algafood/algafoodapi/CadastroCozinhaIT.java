c0a76824eafe5d37cccf6bd8a0d322e39c8a1bc3
aldeir <dide123francisco@gmail.com>
2023-12-05 20:11:46 -0300

    Configurando um banco de testes e usando @TestPropertySource

--- algafood-api/src/test/java/com/algafood/algafoodapi/CadastroCozinhaIT.java
+++ algafood-api/src/test/java/com/algafood/algafoodapi/CadastroCozinhaIT.java
@@ -16,9 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 //webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT =>  vai levantar um servidor web para os teste da API.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
 
     @LocalServerPort
