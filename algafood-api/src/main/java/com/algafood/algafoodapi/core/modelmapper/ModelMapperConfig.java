c37d311160a4f454495d1288d6e41440bd44c11b
Aldeir <dide123francisco@gmail.com>
2024-01-15 20:31:50 -0300

    Customizando o mapeamento de propriedades com ModelMapper

--- algafood-api/src/main/java/com/algafood/algafoodapi/core/modelmapper/ModelMapperConfig.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/core/modelmapper/ModelMapperConfig.java
@@ -4,11 +4,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algafood.algafoodapi.api.model.dtooutput.RestauranteDTO;
+import com.algafood.algafoodapi.domain.model.Restaurante;
+
 @Configuration
 public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
+        // customizando o mapeamento de propriedade com ModelMapper
+        // var modelMapper = new ModelMapper();
+        // modelMapper.createTypeMap(Restaurante.class, RestauranteDTO.class)
+        // .addMapping(Restaurante::getTaxaFrete, RestauranteDTO::setprecoFrete);
+        // return modelMapper;
         return new ModelMapper();
     }
 }
