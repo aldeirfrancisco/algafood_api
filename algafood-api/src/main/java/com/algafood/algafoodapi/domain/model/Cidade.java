b40a9aa49ae80e2490f5076d2473f56b281a9443
Aldeir <dide123francisco@gmail.com>
2024-01-20 09:18:45 -0300

    limpando o condigo

--- algafood-api/src/main/java/com/algafood/algafoodapi/domain/model/Cidade.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/domain/model/Cidade.java
@@ -1,11 +1,15 @@
 package com.algafood.algafoodapi.domain.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
@@ -23,10 +27,15 @@ public class Cidade {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
     private String nome;
 
+    @Valid
     @ConvertGroup(from = Default.class, to = Groups.EstadoId.class)
+    @NotNull
     @ManyToOne
-    @JoinColumn(name = "estado_id", nullable = false)
+    @JoinColumn(nullable = false)
     private Estado estado;
 }
