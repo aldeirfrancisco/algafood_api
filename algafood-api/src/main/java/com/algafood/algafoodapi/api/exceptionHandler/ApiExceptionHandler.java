e5b47ef7f535a8432010b81722316f8c13c5d0ff
Aldeir <dide123francisco@gmail.com>
2024-01-28 12:30:18 -0300

    limpando

--- algafood-api/src/main/java/com/algafood/algafoodapi/api/exceptionHandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/api/exceptionHandler/ApiExceptionHandler.java
@@ -23,7 +23,6 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.algafood.algafoodapi.core.validation.ValidacaoException;
 import com.algafood.algafoodapi.domain.Exception.EntidadeEmUsoException;
 import com.algafood.algafoodapi.domain.Exception.EntidadeNaoEncontradaException;
 import com.algafood.algafoodapi.domain.Exception.NegocioException;
