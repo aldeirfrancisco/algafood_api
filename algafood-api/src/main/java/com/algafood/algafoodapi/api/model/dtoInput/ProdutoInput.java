81593b661111c366dc267cc4ef3fdd93553fbd5d
Aldeir <dide123francisco@gmail.com>
2024-02-03 09:11:43 -0300

    implementando os endpoints de produtos

---
+++ algafood-api/src/main/java/com/algafood/algafoodapi/api/model/dtoInput/ProdutoInput.java
@@ -0,0 +1,27 @@
+package com.algafood.algafoodapi.api.model.dtoInput;
+
+import java.math.BigDecimal;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ProdutoInput {
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String descricao;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal preco;
+
+    @NotNull
+    private Boolean ativo;
+}
---
+++ algafood-api/src/main/java/com/algafood/algafoodapi/domain/service/CadastroProdutoService.java
@@ -0,0 +1,27 @@
+package com.algafood.algafoodapi.domain.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algafood.algafoodapi.domain.Exception.ProdutoNaoEncontradoException;
+import com.algafood.algafoodapi.domain.model.Produto;
+import com.algafood.algafoodapi.domain.repository.ProdutoRepository;
+
+@Service
+public class CadastroProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Transactional
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto buscarOuFalhar(Long restauranteId, Long produtoId) {
+        return produtoRepository.findById(restauranteId, produtoId)
+                .orElseThrow(() -> new ProdutoNaoEncontradoException(restauranteId, produtoId));
+    }
+}
