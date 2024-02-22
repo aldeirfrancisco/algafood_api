54066d1b81dda6d6642e66a3d43e546f0f75f5eb
Aldeir <dide123francisco@gmail.com>
2024-02-22 20:54:04 -0300

    Endpoint de status pedidos cancelar e entregar.

--- algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/FluxoPedidoController.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/FluxoPedidoController.java
@@ -1,9 +1,11 @@
 package com.algafood.algafoodapi.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algafood.algafoodapi.domain.service.FluxoPedidoService;
@@ -20,4 +22,15 @@ public class FluxoPedidoController {
         fluxoPedidoService.confirmar(pedidoId);
     }
 
+    @PutMapping("/cancelamento")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void cancelar(@PathVariable Long pedidoId) {
+        fluxoPedidoService.cancelar(pedidoId);
+    }
+
+    @PutMapping("/entrega")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void entregar(@PathVariable Long pedidoId) {
+        fluxoPedidoService.entregar(pedidoId);
+    }
 }
