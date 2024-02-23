2e81db2c6c3801225bd9f7730d9f7e2a2f916834
Aldeir <dide123francisco@gmail.com>
2024-02-22 21:15:39 -0300

    Refatorando o código de regras para transição de status de pedidos

--- algafood-api/src/main/java/com/algafood/algafoodapi/domain/model/Pedido.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/domain/model/Pedido.java
@@ -20,6 +20,8 @@ import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.algafood.algafoodapi.domain.exception.NegocioException;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -83,4 +85,29 @@ public class Pedido {
         getItens().forEach(item -> item.setPedido(this));
     }
 
+    public void confirmado() {
+        setStatus(StatusPedido.CONFIRMADO);
+        setDataConfirmacao(OffsetDateTime.now());
+    }
+
+    public void cancelar() {
+        setStatus(StatusPedido.CANCELADO);
+        setDataConfirmacao(OffsetDateTime.now());
+    }
+
+    public void entregar() {
+        setStatus(StatusPedido.ENTREGUE);
+        setDataConfirmacao(OffsetDateTime.now());
+    }
+
+    private void setStatus(StatusPedido novoStatus) {
+
+        if (getStatus().naoPodeAlterarPara(novoStatus)) {
+            throw new NegocioException(
+                    String.format("Status do pedido %d não pode ser alterado de %s para %s",
+                            getId(), getStatus().getDescricao(),
+                            novoStatus.getDescricao()));
+        }
+        this.status = novoStatus;
+    }
 }
--- algafood-api/src/main/java/com/algafood/algafoodapi/domain/service/FluxoPedidoService.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/domain/service/FluxoPedidoService.java
@@ -1,14 +1,10 @@
 package com.algafood.algafoodapi.domain.service;
 
-import java.time.OffsetDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.algafood.algafoodapi.domain.exception.NegocioException;
 import com.algafood.algafoodapi.domain.model.Pedido;
-import com.algafood.algafoodapi.domain.model.StatusPedido;
 
 @Service
 public class FluxoPedidoService {
@@ -19,45 +15,19 @@ public class FluxoPedidoService {
     @Transactional
     public void confirmar(Long pedidoId) {
         Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
-
-        if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
-
-            throw new NegocioException(
-                    String.format("Status do pedido %d não pode ser alterado de %s para %s",
-                            pedido.getId(), pedido.getStatus(), StatusPedido.CONFIRMADO.getDescricao()));
-        }
-
-        pedido.setStatus(StatusPedido.CONFIRMADO);
-        pedido.setDataConfirmacao(OffsetDateTime.now());
+        pedido.confirmado();
     }
 
     @Transactional
     public void cancelar(Long pedidoId) {
         Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
-
-        if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
-            throw new NegocioException(
-                    String.format("Status do pedido %d não pode ser alterado de %s para %s",
-                            pedido.getId(), pedido.getStatus().getDescricao(),
-                            StatusPedido.CANCELADO.getDescricao()));
-        }
-
-        pedido.setStatus(StatusPedido.CANCELADO);
-        pedido.setDataCancelamento(OffsetDateTime.now());
+        pedido.cancelar();
     }
 
     @Transactional
     public void entregar(Long pedidoId) {
         Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
+        pedido.entregar();
 
-        if (!pedido.getStatus().equals(StatusPedido.CONFIRMADO)) {
-            throw new NegocioException(
-                    String.format("Status do pedido %d não pode ser alterado de %s para %s",
-                            pedido.getId(), pedido.getStatus().getDescricao(),
-                            StatusPedido.ENTREGUE.getDescricao()));
-        }
-
-        pedido.setStatus(StatusPedido.ENTREGUE);
-        pedido.setDataEntrega(OffsetDateTime.now());
     }
 }
