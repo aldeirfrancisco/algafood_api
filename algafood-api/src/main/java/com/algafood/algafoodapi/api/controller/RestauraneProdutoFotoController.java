bc5a4e28d9b1f15c852250aeda011b865b0df014
Aldeir <dide123francisco@gmail.com>
2024-03-03 21:58:22 -0300

    Validando o tamanho máximo do arquivo

--- algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/RestauraneProdutoFotoController.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/api/controller/RestauraneProdutoFotoController.java
@@ -3,6 +3,8 @@ package com.algafood.algafoodapi.api.controller;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,7 +19,7 @@ public class RestauraneProdutoFotoController {
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable long produtoId,
-            FotoProdutoInput fotoProdutoInput) {
+            @Valid FotoProdutoInput fotoProdutoInput) {
 
         var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
         var arquivoFoto = Path.of("C:/Users/dide1/OneDrive/Área de Trabalho/print/upload", nomeArquivo);
--- algafood-api/src/main/java/com/algafood/algafoodapi/api/model/dtoInput/FotoProdutoInput.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/api/model/dtoInput/FotoProdutoInput.java
@@ -1,6 +1,11 @@
 package com.algafood.algafoodapi.api.model.dtoInput;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algafood.algafoodapi.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +14,9 @@ import lombok.Setter;
 @Setter
 public class FotoProdutoInput {
 
+    @NotNull
+    @FileSize(max = "62KB")
     private MultipartFile arquivo;
+    @NotBlank
     private String descricao;
 }
