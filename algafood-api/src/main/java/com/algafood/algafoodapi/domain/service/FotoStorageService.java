0a9e40801d540dca3a88aef4e4054ee5bf1e6df3
Aldeir <dide123francisco@gmail.com>
2024-03-05 18:55:18 -0300

    Implementando o serviço de armazenagem de fotos no disco local

---
+++ algafood-api/src/main/java/com/algafood/algafoodapi/domain/service/FotoStorageService.java
@@ -0,0 +1,18 @@
+package com.algafood.algafoodapi.domain.service;
+
+import java.io.InputStream;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public interface FotoStorageService {
+
+    void armazenar(NovaFoto novaFoto);
+
+    @Builder
+    @Getter
+    class NovaFoto {
+        private String nomeArquivo;
+        private InputStream inputStriam;
+    }
+}
