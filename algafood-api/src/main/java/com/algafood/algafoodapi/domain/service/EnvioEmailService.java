fd49b75dd49e0dca669f550ed3288cfc698a1411
Aldeir <dide123francisco@gmail.com>
2024-03-16 10:00:05 -0300

    Usando o serviço de envio de e-mails na confirmação de pedidos

--- algafood-api/src/main/java/com/algafood/algafoodapi/domain/service/EnvioEmailService.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/domain/service/EnvioEmailService.java
@@ -2,17 +2,22 @@ package com.algafood.algafoodapi.domain.service;
 
 import java.util.Set;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NonNull;
+import lombok.Singular;
 
 public interface EnvioEmailService {
 
     void envio(Mensagem ms);
 
-    @Setter
+    @Builder
     @Getter
     class Mensagem {
+
+        @Singular
         private Set<String> destinatarios;
+        @NonNull
         private String assunto;
         private String corpo;
     }
--- algafood-api/src/main/java/com/algafood/algafoodapi/domain/service/FluxoPedidoService.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/domain/service/FluxoPedidoService.java
@@ -5,17 +5,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algafood.algafoodapi.domain.model.Pedido;
+import com.algafood.algafoodapi.domain.service.EnvioEmailService.Mensagem;
 
 @Service
 public class FluxoPedidoService {
 
     @Autowired
-    EmissaoPedidoService emissaoPedidoService;
+    private EmissaoPedidoService emissaoPedidoService;
+
+    @Autowired
+    private EnvioEmailService envioEmail;
 
     @Transactional
     public void confirmar(Long pedidoId) {
         Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
         pedido.confirmado();
+        var msn = Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome())
+                .corpo("O pedido de código <strong>" + pedido.getId() + "</strong> foi confirmado")
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+        envioEmail.envio(msn);
     }
 
     @Transactional
