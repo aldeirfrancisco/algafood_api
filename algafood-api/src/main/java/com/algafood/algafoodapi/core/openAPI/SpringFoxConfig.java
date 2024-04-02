81f4c6212268720ff8c936ec8de3cea7ab56c278
Aldeir <dide123francisco@gmail.com>
2024-04-02 20:00:27 -0300

    liimpando inporte

--- algafood-api/src/main/java/com/algafood/algafoodapi/core/openAPI/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algafood/algafoodapi/core/openAPI/SpringFoxConfig.java
@@ -14,7 +14,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
-import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Tag;
@@ -22,7 +21,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import springfox.documentation.service.Response;
-import springfox.documentation.service.ResponseMessage;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
